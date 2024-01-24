3f3f99dd187668d8717e19f537fca7805b49a8e0
Tim White <walton.timothy.james.white@gmail.com>
2024-01-24 13:08:10 +1300

    Copy of test-commons-io-apache repo

---
+++ src/main/java/wtwhite/CommonsIoTest/App.java
@@ -0,0 +1,13 @@
+package wtwhite.CommonsIoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
